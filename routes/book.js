897e9529350b051ef675b2a0c06396da270c7c08
athrys5 <capriotti.andrea00@gmail.com>
2022-11-16 16:21:37 +0100

    big commit

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../models/services')
+const Users = require('../models/customer')
 const bodyParser = require('body-parser');
 router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -8,35 +9,94 @@ router.use(bodyParser.urlencoded({ extended: true })); // for parsing applicatio
 router.get('/book', async (req, res) =>{
     try{  
         const list = await Book.find({});
-        res.render('book.ejs', { data: list })
+        const result = await Users.find({})
+        res.render('book.ejs', { data: list , account: result})
     }catch{ 
     }
 })
 
-router.post('/selectname', async (req, res) => {
+router.post('/book', async (req, res) => {
     try {
         const result = await Book.find({ name: req.body.name })
-        res.render('book.ejs', { data: result })
+        const list = await Users.find({})
+        res.render('book.ejs', { data: result, account: list })
     } catch (error) {
         
     }
 })
 
-router.post('/book', async (req, res) => {
+router.post('/booking', async (req, res) => {
     try {
         const newdate = new Date(req.body.booking)
         const filter = { name: req.body.site }
         const options = { upsert: false }
         const updateDoc = {
             $push: {
-                [`resDog.${req.body.numResDog - 1}`]: {'date': newdate.toISOString(), 'id': 'ciao@gmail.com'},
+                [`resDog.${req.body.numResDog - 1}`]: {'date': newdate.toISOString(), 'id': req.body.usere},
             }
         }
+        console.log(filter, options, updateDoc)
         await Book.updateMany(filter, updateDoc, options);
         res.redirect('/book')
     } catch (error) {
         
     } 
+})
+
+router.post('/deletebooking', async (req, res) => {
+    try {
+        const filter = { name: req.body.sitedel }
+        var fields = req.body.deletedate.split(', ')
+        fields[2] = parseInt(fields[2]) - 1
+        const options = { upsert: false }
+        const updateDoc = {
+            $pull: {
+                [`resDog.${fields[2]}`]: {'date': fields[1], 'id': fields[0]},
+            }
+        }
+        console.log(filter, updateDoc, options)
+        await Book.updateMany(filter, updateDoc, options)
+        res.redirect('/book')
+    } catch (error) {
+        
+    }
+})
+
+router.post('/bookingvet', async (req, res) => {
+    try {
+        const newdate = new Date(req.body.bookingv)
+        const filter = { name: req.body.sitev }
+        const options = { upsert: false }
+        const updateDoc = {
+            $push: {
+                [`resVet.${req.body.numResVet - 1}`]: {'date': newdate.toISOString(), 'id': req.body.userv},
+            }
+        }
+        console.log(filter, options, updateDoc)
+        await Book.updateMany(filter, updateDoc, options);
+        res.redirect('/book')
+    } catch (error) {
+        
+    } 
+})
+
+router.post('/deletebookingvet', async (req, res) => {
+    try {
+        const filter = { name: req.body.sitedelv }
+        var fields = req.body.deletedatev.split(', ')
+        fields[2] = parseInt(fields[2]) - 1
+        const options = { upsert: false }
+        const updateDoc = {
+            $pull: {
+                [`resVet.${fields[2]}`]: {'date': fields[1], 'id': fields[0]},
+            }
+        }
+        console.log(filter, updateDoc, options)
+        await Book.updateMany(filter, updateDoc, options)
+        res.redirect('/book')
+    } catch (error) {
+        
+    }
 })
 
 module.exports = router
